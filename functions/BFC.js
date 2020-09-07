104107251dc371df9ef93047c3a5014bf46a73e7
guillermo-politecnico <58904456+guillermo-politecnico@users.noreply.github.com>
2020-09-08 00:11:04 +0200

    Update BFC.js

--- functions/BFC.js
+++ functions/BFC.js
@@ -7,25 +7,34 @@ const app = express();
 app.use(cors({
     origin: "*"
 }))
-
+// C is the control group
+app.get('/group', (req, res) => {
+    res.send(['A','C'][Math.floor(Math.random()*2)])
+})
 
 app.post("/track", async (req,res) => {
+    console.log(req.body)
     try {
         const {value, userID, action} = req.body;
         switch(action){
             case 'survey':
-                const {genre, age, education, income, country} = value;
-                const studyGroup = ['A','B','C'][Math.floor(Math.random()*3)]
+                const {genre, age, education, income, country, studyUserID, studyGroup} = value;
                 
                 // write to firestore
                 admin.firestore().collection("users").doc(userID).set({
-                    genre: parseInt(genre) || -1, 
-                    age: parseInt(age) || -1, 
-                    education: parseInt(education) || -1, 
-                    income: parseInt(income) || -1, 
-                    studyGroup,
+                    genre: genre || -1, 
+                    age: age || -1, 
+                    education: education || -1, 
+                    income: income || -1, 
+                    studyGroup:studyGroup || -1,
+                    studyUserID: studyUserID|| -1,
                     country: country || -1
                 })
+
+                admin.firestore().collection('users').doc(userID).collection('events').doc().set({
+                    eventName:'start_shopping',
+                    timestamp: new Date().getTime(),
+                });
 
                 res.send(studyGroup);
                 return;
@@ -41,39 +50,50 @@ app.post("/track", async (req,res) => {
                     cartSize,
                     cartPrice: value.reduce((sum, p) => sum + (parseFloat(p.price) * (p.quantity || 1) * (p.currency === 'chf' ? .91 : 1)), 0),
                     cartAvgNutriscore: ['A','B','C','D','E'][Math.round(averageNutriscore)],
-                    cartItems: value.map(p => ({...p, quantity: p.quantity || 1}))
+                    cartItems: value.map(p => ({...p, quantity: p.quantity || 1, size: p.size || false}))
                 }, { merge: true });
-                
+
+                admin.firestore().collection('users').doc(userID).collection('events').doc().set({
+                    eventName:'finish_shopping',
+                    timestamp: new Date().getTime(),
+                });
+
                 res.send({ok:true})
                 return;
 
             case 'track_page':
-                const {category, timeSpent, pageUrl, title} = value;
+                const {category, timeSpent, pageUrl, title, gtin,nutri_score, nutritionTable} = value;
 
                 admin.firestore().collection('users').doc(userID).collection('events').doc().set({
                     eventName:'page_view',
                     timestamp: new Date().getTime(),
-                    timeSpent,
-                    category,
-                    pageUrl,
-                    title
+                    timeSpent: timeSpent || false,
+                    category: category || false,
+                    gtin: gtin || false,
+                    nutritionTable: nutritionTable || false,
+                    nutri_score: nutri_score || false,
+                    pageUrl: pageUrl || false,
+                    title: title || false
                 });
                 res.end()
                 return
             case 'track_cart':
                 const {product, add_remove} = value;
+                console.log(product)
 
                 admin.firestore().collection('users').doc(userID).collection('events').doc().set({
                     eventName:add_remove === 'add' ? 'add_to_cart' : 'remove_from_cart',
                     timestamp: new Date().getTime(),
                     product: {
-                        currency: product.currency,
-                        gtin: product.gtin,
-                        category: product.category,
-                        name: product.name.trim(),
-                        price: parseFloat(product.price),
-                        img: product.img,
-                        nutriScore: product.nutriScore
+                        currency: product.currency || false,
+                        gtin: product.gtin|| false,
+                        category: product.category|| false,
+                        name: product.name|| false,
+                        nutritionTable: product.nutritionTable || false,
+                        size: product.size|| false,
+                        price: parseFloat(product.price)|| false,
+                        img: product.img|| false,
+                        nutriScore: product.nutriScore|| false
                     }
                 });
                 res.end()
